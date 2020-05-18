13320367e9dbb118bfa0c457d43ca8158a5d1e23
Ayanami95 <65207268+Ayanami95@users.noreply.github.com>
2020-05-18 22:27:45 +0900

    homework 04

--- 04_simd/13_nbody.cpp
+++ 04_simd/13_nbody.cpp
@@ -21,29 +21,36 @@ int main() {
   __m256 one = _mm256_set1_ps(1);
   
   for(int i=0; i<N; i++){
-	  __m256 xi = _mm256_set1_ps(x[i]);
-	  __m256 yi = _mm256_set1_ps(y[i]);
-	  __m256 rx = _mm256_sub_ps(xi, xvec);
-	  __m256 ry = _mm256_sub_ps(yi, yvec);
-	  __m256 rx2 = _mm256_mul_ps(rx, rx);
-	  __m256 ry2 = _mm256_mul_ps(ry, ry);
-	  __m256 r2 = _mm256_add_ps(rx2, ry2);
+    __m256 xi = _mm256_set1_ps(x[i]);
+    __m256 yi = _mm256_set1_ps(y[i]);
+    __m256 rx = _mm256_sub_ps(xi, xvec);
+    __m256 ry = _mm256_sub_ps(yi, yvec);
+    __m256 rx2 = _mm256_mul_ps(rx, rx);
+    __m256 ry2 = _mm256_mul_ps(ry, ry);
+    __m256 r2 = _mm256_add_ps(rx2, ry2);
 	  
-	  // To avoid the situation when r2 = 0
-	  __m256 mask = _mm256_cmp_ps(r2, zero, _CMP_GT_OQ);
-	  r2 = _mm256_belndv_ps(one, r2, mask);
+    // To avoid the situation when r2 = 0
+    __m256 mask = _mm256_cmp_ps(r2, zero, _CMP_GT_OQ);
+    r2 = _mm256_belndv_ps(one, r2, mask);
 	  
-	  __m256 r = _mm256_rsqrt_ps(r2);
-	  r2 = _mm256_mul_ps(r, r);
-	  __m256 r3 = _mm256_mul_ps(r2, r);
+    __m256 r = _mm256_rsqrt_ps(r2);
+    r2 = _mm256_mul_ps(r, r);
+    __m256 r3 = _mm256_mul_ps(r2, r);
 	  
-	  fxvec = _mm256_mul_ps(rx, mvec);
-	  fxvec = _mm256_mul_ps(fxvec, r3);
-	  fyvec = _mm256_mul_ps(ry, mvec);
-	  fyvec = _mm256_mul_ps(fyvec, r3);
-	  
-	  printf("%d %g %g\n",i,fx[i],fy[i]);
-	  }
+    __m256 temp = _mm256_mul_ps(rx, mvec);
+    temp = _mm256_mul_ps(temp, r3);
+    fxvec = _mm256_sub_ps(fxvec, temp);
+    temp = _mm256_mul_ps(ry, mvec);
+    temp = _mm256_mul_ps(temp, r3);
+    fyvec = _mm256_sub_ps(fyvec, temp);
+  }
+
+  _mm256_store_ps(fx, fxvec);
+  _mm256_store_ps(fy, fyvec);
+
+  for(int i=0; i<N; i++)
+    printf("%d %g %g\n",i,fx[i],fy[i]);
+
   /*for(int i=0; i<N; i++) {
     for(int j=0; j<N; j++) {
       if(i != j) {
