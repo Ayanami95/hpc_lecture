3bd03687b885c8f71d42e4be09fd931fcb990c7a
Ayanami95 <65207268+Ayanami95@users.noreply.github.com>
2020-06-28 23:10:16 +0900

    final_report

--- final_report/final_1Dmatrix.cpp
+++ final_report/final_1Dmatrix.cpp
@@ -1,157 +1,145 @@
-﻿#include <iostream>
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <iomanip>
 #include <fstream>
-#include<vector>
+#include <sstream>
+#include <string>
+#include <algorithm>
+
 using namespace std;
-typedef vector<vector<float>> Matrix;
 
+const int nx=41;   
+const int ny=41;  
+const int nt=100;  
+const int nit=50;  
+const int size = nx * ny;
 
-Matrix build_up_b(Matrix b, float rho, float dt, Matrix u, Matrix v, float dx, float dy) {
-    int row = b.size();
-    int col = b[0].size();
+const double dx=2.0/(nx-1);
+const double dy=2.0/(ny-1);
 
-    for (int m = 0; m < row - 2; m++) {
-        for (int n = 0; n < col - 2; n++) {
-            float dudx = (u[m + 1][n + 2] - u[m + 1][n]) / (2.0 * dx);
-            float dvdy = (v[m + 2][n + 1] - v[m][n + 1]) / (2.0 * dy);
-            float dudy = (u[m + 2][n + 1] - u[m][n + 1]) / (2.0 * dy);
-            float dvdx = (v[m + 1][n + 2] - v[m + 1][n]) / (2.0 * dx);
+const double rho=1;
+const double nu=0.1;
+const double dt=0.001;
 
-            b[m + 1][n + 1] = rho * 1 / dt * (dudx + dvdy) - dudx * dudx - 2 * dudy * dvdx - dvdy * dvdy;
+void build_up_b(double b[], double u[],double v[]){
+    for(int i = 1; i < nx - 1; i++){
+        for(int j = 1; j < ny - 1; j++){
+            b[j*nx+i] = (rho * (1.0/dt * ((u[j*nx + i+1] - u[j*nx + i-1]) 
+            / (2 * dx) + (v[(j+1)*nx+i] - v[(j-1)*nx+i]) / (2 * dy)) - ((u[j*nx+i+1] - u[j*nx+i-1]) 
+            / (2*dx)) * ((u[j*nx+i+1] - u[j*nx+i-1]) / (2*dx)) - 2 * ((u[(j+1)*nx+i] - u[(j-1)*nx+i]) 
+            / (2*dy) * (v[j*nx+i+1] - v[j*nx + i-1]) / (2*dx)) - ((v[(j+1)*nx+i] - v[(j-1)*nx+i]) 
+            / (2*dy)) * ((v[(j+1)*nx+i] - v[(j-1)*nx+i]) / (2*dy)) ));
         }
     }
-    return b;
 }
-Matrix pressure_poisson(Matrix p, float dx, float dy, Matrix b, int nit) {
-    int row = p.size();
-    int col = p[0].size();
-    Matrix pn(row, vector<float>(col));
-    for (int k = 0; k < nit; k++) {
-        pn = p;
 
-        for (int m = 0; m < row - 2; m++) {
-            for (int n = 0; n < col - 2; n++) {
-                //$$p_{i,j}^{n}=\frac{(p_{i+1,j}^{n}+p_{i-1,j}^{n})\Delta y^2+(p_{i,j+1}^{n}+p_{i,j-1}^{n})\Delta x^2-b_{i,j}^{n}\Delta x^2\Delta y^2}{2(\Delta x^2+\Delta y^2)}$$
-                p[m + 1][n + 1] = ((pn[m + 1][n + 2] + pn[m + 1][n]) * dy * dy
-                    + (pn[m + 2][n + 1] + pn[m][n + 1]) * dx * dx
-                    - b[m + 1][n + 1] * dx * dx * dy * dy)
-                    / (2 * (dx * dx + dy * dy));
+void pressure_poisson(double p[],double pn[], double b[]){
+   int i, j;
+    for(int k = 0; k < nit ; k++){
+        for(i = 0; i < nx ; i++){
+            for(j = 0; j < ny ; j++){
+                pn[j*nx+i] = p[j*nx+i];
             }
         }
-        //dp/dx = 0 at x = 2 and dp/dx = 0 at x = 0
-        for (int m = 0; m < row; m++) {
-            p[m][col - 1] = p[m][col - 2];
-            p[m][0] = p[m][1];
-        }
-        //dp/dy = 0 at y = 0 and p = 0 at y = 2
-        for (int n = 0; n < col; n++) {
-            p[0][n] = p[1][n];
-            p[row - 1][n] = 0;
-        }
-
-    }
-    return p;
-}
-void cavity_flow(int nt, Matrix& u, Matrix& v, float dt, float dx, float dy, Matrix& p, float rho, float nu, int nit) {
-    int row = u.size();
-    int col = u[0].size();
-    Matrix b(row, vector<float>(col, 0));
-    Matrix un(row, vector<float>(col));
-    Matrix vn(row, vector<float>(col));
-
-    for (int i = 0; i < nt; i++) {
-        un = u;
-        vn = v;
-        b = build_up_b(b, rho, dt, u, v, dx, dy);
-        p = pressure_poisson(p, dx, dy, b, nit);
-        for (int m = 0; m < row - 2; m++) {
-            for (int n = 0; n < col - 2; n++) {
-                u[m + 1][n + 1] = (un[m + 1][n + 1]
-                    - un[m + 1][n + 1] * dt / dx * (un[m + 1][n + 1] - un[m + 1][n])
-                    - vn[m + 1][n + 1] * dt / dy * (un[m + 1][n + 1] - un[m][n + 1])
-                    - dt / (2 * rho * dx) * (p[m + 1][n + 2] - p[m + 1][n])
-                    + nu * (dt / (dx * dx) * (un[m + 1][n + 2] - 2 * un[m + 1][n + 1] + un[m + 1][n])
-                        + dt / (dy * dy) * (un[m + 2][n + 1] - 2 * un[m + 1][n + 1] + un[m][n + 1])));
-
-                v[m + 1][n + 1] = (vn[m + 1][n + 1]
-                    - un[m + 1][n + 1] * dt / dx * (vn[m + 1][n + 1] - vn[m + 1][n])
-                    - vn[m + 1][n + 1] * dt / dy * (vn[m + 1][n + 1] - vn[m][n + 1])
-                    - dt / (2 * rho * dx) * (p[m + 2][n + 1] - p[m + 0][n+1])
-                    + nu * (dt / (dx * dx) * (vn[m + 1][n + 2] - 2 * vn[m + 1][n + 1] + vn[m + 1][n])
-                        + dt / (dy * dy) * (vn[m + 2][n + 1] - 2 * vn[m + 1][n + 1] + vn[m][n + 1])));
+        for(i = 1; i < nx - 1 ; i++){
+            for(j = 1; j < ny - 1; j++){
+                p[j*nx+i] = (((pn[j*nx+i+1] + pn[j*nx+i-1]) * dy * dy + (pn[(j+1)*nx+i] + pn[(j-1)*nx+i]) * dx * dx)
+                / (2 * (dx * dx + dy * dy)) - dx * dx * dy * dy * b[j*nx+i] * rho / (2 * (dx *dx + dy * dy)));
             }
         }
-        //  u[0,:] =  v[0,:] = v[-1,:] = 0  u[-1,:] =1
-        for (int n = 0; n < col; n++) {
-            u[0][n] = 0;
-            u[row - 1][n] = 1;
-            v[0][n] = 0;
-            v[row - 1][n] = 0;
+        for(int m = 1 ; m < ny - 1 ; m++){
+            p[m*nx+nx-1] = p[m*nx+nx-2]; 
+            p[m*nx+0]    = p[m*nx+1];    
         }
-
-        //  u[:,0] = u[:,-1] = v[:,0] = v[:,-1] = 0
-        for (int m = 0; m < row; m++) {
-            u[m][0] = 0;
-            u[m][col - 1] = 0;
-            v[m][0] = 0;
-            v[m][col - 1] = 0;
+        for(int n = 0 ; n < nx ; n++){
+            p[0*nx+n] = p[1*nx+n]; 
+            p[(nx-1)*nx+n] = 0.0;  
         }
-
     }
 }
 
-void writeFile(Matrix& u, Matrix& v, Matrix& p) {
-    ofstream fs("cavity_cpp_results.txt");
-    int row = u.size();
-    int col = u[0].size();
-    // u
+void cavity_flow(double u[], double un[], double v[], double vn[], double p[], double pn[], double b[]){
+    for(int i = 0; i < nx; i++){
+        for(int j = 0; j < ny; j++){
+            un[j*nx+i] = u[j*nx+i];
+            vn[j*nx+i] = v[j*nx+i];
+        }
+    }
+    
+    build_up_b(b, u, v);
+    pressure_poisson(p, pn, b);
+    
+    for(int i = 1; i < nx - 1; i++){
+        for(int j = 1; j < ny - 1; j++){
+            u[j*nx+i] = (un[j*nx+i] - un[j*nx+i] * dt / dx * (un[j*nx+i] - un[j*nx+i-1]) - vn[j*nx+i] * dt 
+            / dy * (un[j*nx+i] - un[(j-1)*nx+i]) - dt / (2 * rho * dx) * (p[j*nx+i+1] - p[j*nx+i-1]) 
+            + nu * (dt / (dx * dx) * (un[j*nx+i+1] - 2*un[j*nx+i] + un[j*nx+i-1]) + dt / (dy * dy) * (un[(j+1)*nx+i] 
+            - 2*un[j*nx+i] + un[(j-1)*nx+i])));
+
+            v[j*nx+i] = ( vn[j*nx+i] - un[j*nx+i] * dt / dx * (vn[j*nx+i] - vn[j*nx+i-1]) - vn[j*nx+i] * dt 
+            / dy * (vn[j*nx+i] - vn[(j-1)*nx+i]) - dt / (2 * rho * dy) * (p[(j+1)*nx+i] - p[(j-1)*nx+i]) 
+            + nu * (dt / (dx * dx) * (vn[j*nx+i+1] - 2*vn[j*nx+i] + vn[j*nx+i-1]) + dt / (dy * dy) 
+            * (vn[(j+1)*nx+i] - 2*vn[j*nx+i] + vn[(j-1)*nx+i])));
+        }
+    }
+    
+    for(int i = 0 ; i < nx; i++){
+        u[0*nx+i] = 0.0;
+        u[(nx-1)*nx+i] = 1.0;
+        v[0*nx+i] = 0.0;
+        v[(nx-1)*nx+i] = 0.0;
+    }
+    
+    for(int j = 1; j < ny - 1; j++){
+        u[j*nx+0] = 0.0;
+        u[j*nx+nx-1] = 0.0;
+        v[j*nx+0] = 0.0;
+        v[j*nx+nx-1] = 0.0;
+    }
+}
+
+void writeFile(double u[], double v[], double p[]) {
+    ofstream fs("cavity_cpp_results_final.txt");
+
     fs << "u ";
-    for (int m = 0; m < row; m++) {
-        for (int n = 0; n < col; n++) {
-            fs << u[m][n] << " ";
-        }
-        fs << "\n";
+    for (int i = 0; i < size; i++) {
+        fs << u[i] << " ";
     }
     fs << "\nv";
 
-    for (int m = 0; m < row; m++) {
-        for (int n = 0; n < col; n++) {
-            fs << v[m][n] << " ";
-        }
-        fs << "\n";
+    for (int i = 0; i < size; i++) {
+        fs << v[i] << " ";
     }
     fs << "\np";
 
-    for (int m = 0; m < row; m++) {
-        for (int n = 0; n < col; n++) {
-            fs << p[m][n] << " ";
-        }
-        fs << "\n";
+    for (int i = 0; i < size; i++) {
+        fs << p[i] << " ";
     }
     fs << "\n";
 
     fs.close();
 }
 
-int main()
-{
-    int nx = 41;
-    int ny = 41;
-    int nt = 100;
-    int nit = 50;
+int main(void){
+    double u[size] = {0.0};
+    double un[size] = {0.0};
+    double v[size] = {0.0};
+    double vn[size] = {0.0};
+    double p[size] = {0.0};
+    double pn[size] = {0.0};
+    double b[size] = {0.0};
 
-    float c = 1;
-    float dx = 2.0 / (nx - 1.0);
-    float dy = 2.0 / (ny - 1.0);
+    for(int i = 0; i < nx; i++){
+        u[(nx-1)*nx + i] = 1.0;
+        un[(nx-1)*nx + i] = 1.0;
+    }
 
-    float rho = 1;
-    float nu = 0.1;
-    float dt = 0.001;
+    for(int i=1;i<nt+1;i++){
+        cavity_flow(u,un,v,vn,p,pn,b); 
+    }
 
-    Matrix u(ny, vector<float>(nx, 0));
-    Matrix v(ny, vector<float>(nx, 0));
-    Matrix p(ny, vector<float>(nx, 0));
+    writeFile(u,v,p);
 
-    cavity_flow(nt, u, v, dt, dx, dy, p, rho, nu, nit);
-    writeFile(u, v, p);
+return 0;
 }
-
--- final_report/final_2Dmatrix.cpp
+++ final_report/final_2Dmatrix.cpp
@@ -1,162 +1,157 @@
 #include <iostream>
-#include <cmath>
-#include <cstdlib>
-#include <iomanip>
 #include <fstream>
-#include <sstream>
-#include <string>
-#include <algorithm>
-
+#include<vector>
 using namespace std;
+typedef vector<vector<float>> Matrix;
 
-const int nx=41;   
-const int ny=41;  
-const int nt=100;  
-const int nit=50;  
-const int size = nx * ny;
 
-const double dx=2.0/(nx-1);
-const double dy=2.0/(ny-1);
+Matrix build_up_b(Matrix b, float rho, float dt, Matrix u, Matrix v, float dx, float dy) {
+    int row = b.size();
+    int col = b[0].size();
 
-const double rho=1;
-const double nu=0.1;
-const double dt=0.001;
+    for (int m = 0; m < row - 2; m++) {
+        for (int n = 0; n < col - 2; n++) {
+            float dudx = (u[m + 1][n + 2] - u[m + 1][n]) / (2.0 * dx);
+            float dvdy = (v[m + 2][n + 1] - v[m][n + 1]) / (2.0 * dy);
+            float dudy = (u[m + 2][n + 1] - u[m][n + 1]) / (2.0 * dy);
+            float dvdx = (v[m + 1][n + 2] - v[m + 1][n]) / (2.0 * dx);
 
-void build_up_b(double b[], double u[],double v[]){
-    for(int i = 1; i < nx - 1; i++){
-        for(int j = 1; j < ny - 1; j++){
-            b[j*nx+i] = (rho * (1.0/dt * ((u[j*nx + i+1] - u[j*nx + i-1]) 
-            / (2 * dx) + (v[(j+1)*nx+i] - v[(j-1)*nx+i]) / (2 * dy)) - ((u[j*nx+i+1] - u[j*nx+i-1]) 
-            / (2*dx)) * ((u[j*nx+i+1] - u[j*nx+i-1]) / (2*dx)) - 2 * ((u[(j+1)*nx+i] - u[(j-1)*nx+i]) 
-            / (2*dy) * (v[j*nx+i+1] - v[j*nx + i-1]) / (2*dx)) - ((v[(j+1)*nx+i] - v[(j-1)*nx+i]) 
-            / (2*dy)) * ((v[(j+1)*nx+i] - v[(j-1)*nx+i]) / (2*dy)) ));
+            b[m + 1][n + 1] = rho * 1 / dt * (dudx + dvdy) - dudx * dudx - 2 * dudy * dvdx - dvdy * dvdy;
         }
     }
+    return b;
 }
+Matrix pressure_poisson(Matrix p, float dx, float dy, Matrix b, int nit) {
+    int row = p.size();
+    int col = p[0].size();
+    Matrix pn(row, vector<float>(col));
+    for (int k = 0; k < nit; k++) {
+        pn = p;
 
-void pressure_poisson(double p[],double pn[], double b[]){
-   int i, j;
-    for(int k = 0; k < nit ; k++){
-        for(i = 0; i < nx ; i++){
-            for(j = 0; j < ny ; j++){
-                pn[j*nx+i] = p[j*nx+i];
+        for (int m = 0; m < row - 2; m++) {
+            for (int n = 0; n < col - 2; n++) {
+                //$$p_{i,j}^{n}=\frac{(p_{i+1,j}^{n}+p_{i-1,j}^{n})\Delta y^2+(p_{i,j+1}^{n}+p_{i,j-1}^{n})\Delta x^2-b_{i,j}^{n}\Delta x^2\Delta y^2}{2(\Delta x^2+\Delta y^2)}$$
+                p[m + 1][n + 1] = ((pn[m + 1][n + 2] + pn[m + 1][n]) * dy * dy
+                    + (pn[m + 2][n + 1] + pn[m][n + 1]) * dx * dx
+                    - b[m + 1][n + 1] * dx * dx * dy * dy)
+                    / (2 * (dx * dx + dy * dy));
             }
         }
-        for(i = 1; i < nx - 1 ; i++){
-            for(j = 1; j < ny - 1; j++){
-                p[j*nx+i] = (((pn[j*nx+i+1] + pn[j*nx+i-1]) * dy * dy + (pn[(j+1)*nx+i] + pn[(j-1)*nx+i]) * dx * dx)
-                / (2 * (dx * dx + dy * dy)) - dx * dx * dy * dy * b[j*nx+i] * rho / (2 * (dx *dx + dy * dy)));
+        //dp/dx = 0 at x = 2 and dp/dx = 0 at x = 0
+        for (int m = 0; m < row; m++) {
+            p[m][col - 1] = p[m][col - 2];
+            p[m][0] = p[m][1];
+        }
+        //dp/dy = 0 at y = 0 and p = 0 at y = 2
+        for (int n = 0; n < col; n++) {
+            p[0][n] = p[1][n];
+            p[row - 1][n] = 0;
+        }
+
+    }
+    return p;
+}
+void cavity_flow(int nt, Matrix& u, Matrix& v, float dt, float dx, float dy, Matrix& p, float rho, float nu, int nit) {
+    int row = u.size();
+    int col = u[0].size();
+    Matrix b(row, vector<float>(col, 0));
+    Matrix un(row, vector<float>(col));
+    Matrix vn(row, vector<float>(col));
+
+    for (int i = 0; i < nt; i++) {
+        un = u;
+        vn = v;
+        b = build_up_b(b, rho, dt, u, v, dx, dy);
+        p = pressure_poisson(p, dx, dy, b, nit);
+        for (int m = 0; m < row - 2; m++) {
+            for (int n = 0; n < col - 2; n++) {
+                u[m + 1][n + 1] = (un[m + 1][n + 1]
+                    - un[m + 1][n + 1] * dt / dx * (un[m + 1][n + 1] - un[m + 1][n])
+                    - vn[m + 1][n + 1] * dt / dy * (un[m + 1][n + 1] - un[m][n + 1])
+                    - dt / (2 * rho * dx) * (p[m + 1][n + 2] - p[m + 1][n])
+                    + nu * (dt / (dx * dx) * (un[m + 1][n + 2] - 2 * un[m + 1][n + 1] + un[m + 1][n])
+                        + dt / (dy * dy) * (un[m + 2][n + 1] - 2 * un[m + 1][n + 1] + un[m][n + 1])));
+
+                v[m + 1][n + 1] = (vn[m + 1][n + 1]
+                    - un[m + 1][n + 1] * dt / dx * (vn[m + 1][n + 1] - vn[m + 1][n])
+                    - vn[m + 1][n + 1] * dt / dy * (vn[m + 1][n + 1] - vn[m][n + 1])
+                    - dt / (2 * rho * dx) * (p[m + 2][n + 1] - p[m + 0][n+1])
+                    + nu * (dt / (dx * dx) * (vn[m + 1][n + 2] - 2 * vn[m + 1][n + 1] + vn[m + 1][n])
+                        + dt / (dy * dy) * (vn[m + 2][n + 1] - 2 * vn[m + 1][n + 1] + vn[m][n + 1])));
             }
         }
-        for(int m = 1 ; m < ny - 1 ; m++){
-            p[m*nx+nx-1] = p[m*nx+nx-2]; 
-            p[m*nx+0]    = p[m*nx+1];    
+        //  u[0,:] =  v[0,:] = v[-1,:] = 0  u[-1,:] =1
+        for (int n = 0; n < col; n++) {
+            u[0][n] = 0;
+            u[row - 1][n] = 1;
+            v[0][n] = 0;
+            v[row - 1][n] = 0;
         }
-        for(int n = 0 ; n < nx ; n++){
-            p[0*nx+n] = p[1*nx+n]; 
-            p[(nx-1)*nx+n] = 0.0;  
+
+        //  u[:,0] = u[:,-1] = v[:,0] = v[:,-1] = 0
+        for (int m = 0; m < row; m++) {
+            u[m][0] = 0;
+            u[m][col - 1] = 0;
+            v[m][0] = 0;
+            v[m][col - 1] = 0;
         }
+
     }
 }
 
-void cavity_flow(double u[], double un[], double v[], double vn[], double p[], double pn[], double b[]){
-    for(int i = 0; i < nx; i++){
-        for(int j = 0; j < ny; j++){
-            un[j*nx+i] = u[j*nx+i];
-            vn[j*nx+i] = v[j*nx+i];
-        }
-    }
-    
-    build_up_b(b, u, v);
-    pressure_poisson(p, pn, b);
-    
-    for(int i = 1; i < nx - 1; i++){
-        for(int j = 1; j < ny - 1; j++){
-            u[j*nx+i] = (un[j*nx+i] - un[j*nx+i] * dt / dx * (un[j*nx+i] - un[j*nx+i-1]) - vn[j*nx+i] * dt 
-            / dy * (un[j*nx+i] - un[(j-1)*nx+i]) - dt / (2 * rho * dx) * (p[j*nx+i+1] - p[j*nx+i-1]) 
-            + nu * (dt / (dx * dx) * (un[j*nx+i+1] - 2*un[j*nx+i] + un[j*nx+i-1]) + dt / (dy * dy) * (un[(j+1)*nx+i] 
-            - 2*un[j*nx+i] + un[(j-1)*nx+i])));
-
-            v[j*nx+i] = ( vn[j*nx+i] - un[j*nx+i] * dt / dx * (vn[j*nx+i] - vn[j*nx+i-1]) - vn[j*nx+i] * dt 
-            / dy * (vn[j*nx+i] - vn[(j-1)*nx+i]) - dt / (2 * rho * dy) * (p[(j+1)*nx+i] - p[(j-1)*nx+i]) 
-            + nu * (dt / (dx * dx) * (vn[j*nx+i+1] - 2*vn[j*nx+i] + vn[j*nx+i-1]) + dt / (dy * dy) 
-            * (vn[(j+1)*nx+i] - 2*vn[j*nx+i] + vn[(j-1)*nx+i])));
-        }
-    }
-    
-    for(int i = 0 ; i < nx; i++){
-        u[0*nx+i] = 0.0;
-        u[(nx-1)*nx+i] = 1.0;
-        v[0*nx+i] = 0.0;
-        v[(nx-1)*nx+i] = 0.0;
-    }
-    
-    for(int j = 1; j < ny - 1; j++){
-        u[j*nx+0] = 0.0;
-        u[j*nx+nx-1] = 0.0;
-        v[j*nx+0] = 0.0;
-        v[j*nx+nx-1] = 0.0;
-    }
-}
-
-void writeFile(double u[], double v[], double p[]) {
-    ofstream fs("cavity_cpp_results_04.txt");
-
+void writeFile(Matrix& u, Matrix& v, Matrix& p) {
+    ofstream fs("cavity_cpp_results_final.txt");
+    int row = u.size();
+    int col = u[0].size();
+    // u
     fs << "u ";
-    for (int i = 0; i < size; i++) {
-        fs << u[i] << " ";
+    for (int m = 0; m < row; m++) {
+        for (int n = 0; n < col; n++) {
+            fs << u[m][n] << " ";
+        }
+        fs << "\n";
     }
     fs << "\nv";
 
-    for (int i = 0; i < size; i++) {
-        fs << v[i] << " ";
+    for (int m = 0; m < row; m++) {
+        for (int n = 0; n < col; n++) {
+            fs << v[m][n] << " ";
+        }
+        fs << "\n";
     }
     fs << "\np";
 
-    for (int i = 0; i < size; i++) {
-        fs << p[i] << " ";
+    for (int m = 0; m < row; m++) {
+        for (int n = 0; n < col; n++) {
+            fs << p[m][n] << " ";
+        }
+        fs << "\n";
     }
     fs << "\n";
 
     fs.close();
 }
 
-int main(void){
-    double u[size] = {0.0};
-    double un[size] = {0.0};
-    double v[size] = {0.0};
-    double vn[size] = {0.0};
-    double p[size] = {0.0};
-    double pn[size] = {0.0};
-    double b[size] = {0.0};
+int main()
+{
+    int nx = 41;
+    int ny = 41;
+    int nt = 100;
+    int nit = 50;
 
-    for(int i = 0; i < nx; i++){
-        u[(nx-1)*nx + i] = 1.0;
-        un[(nx-1)*nx + i] = 1.0;
-    }
+    float c = 1;
+    float dx = 2.0 / (nx - 1.0);
+    float dy = 2.0 / (ny - 1.0);
 
-  //in(u,un,v,vn,p,pn,b,nx,ny);
-    for(int i=1;i<nt+1;i++){
-        cavity_flow(u,un,v,vn,p,pn,b); 
-    }
+    float rho = 1;
+    float nu = 0.1;
+    float dt = 0.001;
 
-    writeFile(u,v,p);
+    Matrix u(ny, vector<float>(nx, 0));
+    Matrix v(ny, vector<float>(nx, 0));
+    Matrix p(ny, vector<float>(nx, 0));
 
-//Write in CSV
-  //ofstream outFile;
-  //outFile.open("data.csv",ios::out);
-  //for(int i=0;i<nx*ny;i++){
-  //  outFile<<*(u+i)<<','<<*(v+i)<<','<<*(p+i)<<endl;
-  //  }
-  //outFile.close();
-  //cout<<"Finish"<<endl;
-
-  //free(u);
-  //free(un);
-  //free(v);
-  //free(vn);
-  //free(p);
-  //free(pn);
-  //free(b);
-return 0;
+    cavity_flow(nt, u, v, dt, dx, dy, p, rho, nu, nit);
+    writeFile(u, v, p);
 }
+
